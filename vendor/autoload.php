8288379d27f2afba0bf09232cfc717b49df1ca09
Chris007M <windows.congress@gmail.com>
2026-06-19 11:10:16 +0200

    Add PHPMailer source and autoload shim (vendored)

---
+++ vendor/autoload.php
@@ -0,0 +1,1 @@
+﻿<?php\nrequire_once __DIR__.'/phpmailer/phpmailer/src/Exception.php';\nrequire_once __DIR__.'/phpmailer/phpmailer/src/PHPMailer.php';\nrequire_once __DIR__.'/phpmailer/phpmailer/src/SMTP.php';\n
